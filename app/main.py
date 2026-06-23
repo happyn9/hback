e950f803e60d92dcda3d1f45a172ddd104c0a9e4
happyn9 <nephtalitwite12@gmail.com>
2026-06-23 09:50:21 +0200

    hlearning back V1.0.1

--- app/main.py
+++ app/main.py
@@ -66,7 +66,24 @@ app.include_router(payment.router)
 
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
+
+from fastapi import Request
+from fastapi.responses import JSONResponse
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": str(exc)},
+        headers={"Access-Control-Allow-Origin": "*"}
+    )
+
+
+
 # =================== STARTUP ===================
+
+
+
 @app.on_event("startup")
 def startup():
     db = SessionLocal()
